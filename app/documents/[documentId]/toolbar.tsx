54f6ffb9ce3199376027ca41772d64b4cdd69196
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-05-12 19:40:53 +0200

    feat: font family button

--- app/documents/[documentId]/toolbar.tsx
+++ app/documents/[documentId]/toolbar.tsx
@@ -1,9 +1,51 @@
 "use client"
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+
+const FontFamilyButton = () => {
+    const { editor } = useEditorStore();
+
+    const fonts = [
+        { label: "Arial", value: "Arial" },
+        { label: "Times New Roman", value: "Times New Roman" },
+        { label: "Courier New", value: "Courier New" },
+        { label: "Georgia", value: "Georgia" },
+        { label: "Verdana", value: "Verdana" },
+    ];
+
+    return(
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <button
+                    className="h-7 w-[120px] shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <span className="truncate">
+                        {editor?.getAttributes("textStyle").fontFamily || "Arial"}
+                    </span>
+                    <ChevronDownIcon className="size-4 shrink-0 ml-2" /> 
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {fonts.map(({ label, value }) => (
+                    <button
+                        onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+                        key={value}
+                        className={cn(
+                            "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                            editor?.getAttributes("textStyle").fontFamily === value && "bg-neutral-200/80"
+                        )}
+                    >
+                        <span>{label}</span>
+                    </button>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 
 interface ToolbarButtonProps {
     onClick: () => void;
@@ -105,6 +147,10 @@ export default function Toolbar() {
             {sections[0].map((item) => (
                 <ToolbarButton key={item.label} {...item} />
             ))}
+
+            <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+
+            <FontFamilyButton />
 
             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
         
